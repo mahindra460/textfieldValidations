9f9f92422c031ae7b1f9c557b2701deaadbd6292
Mahendra Reddy <mahendrareddy@Mahendras-MacBook-Air.local>
2020-03-14 12:18:28 +0530

    files added

---
+++ DemoGIT/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DemoGIT
+//
+//  Created by Mahendra Reddy on 14/03/20.
+//  Copyright © 2020 Mahendra Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+       print("hai")
+    }
+
+
+}
+
